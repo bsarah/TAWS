e43c8b93670b7934df7855e65f9990ebaabab5c3
Florian Eggenhofer <egg@tbi.univie.ac.at>
2015-12-28 15:06:11 +0100

    Included error message field in template

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -33,83 +33,91 @@ import Data.Either.Unwrap
 -- inclined, or create a single monolithic file.
 getHomeR :: Handler Html
 getHomeR = do
-     (formWidget, formEnctype) <- generateFormPost inputForm
-     (sampleWidget, sampleEnctype) <- generateFormPost sampleForm
-     defaultLayout $ do
-        aDomId <- newIdent
-        setTitle "Welcome To TAWS!"
-        $(widgetFile "homepage") --template
+  (formWidget, formEnctype) <- generateFormPost inputForm
+  (sampleWidget, sampleEnctype) <- generateFormPost sampleForm
+  defaultLayout $ do
+    aDomId <- newIdent
+    let errorMsg = DT.pack "Test message<br>"
+    setTitle "Welcome To TAWS!"
+    $(widgetFile "homepage") 
 
 postHomeR :: Handler Html
 postHomeR = do
-    ((formResult, _), _) <- runFormPost inputForm
-    ((sampleResult,_),_) <- runFormPost sampleForm
-    app <- getYesod -- contains all the generally set path variables
-    --Create tempdir and session-Id
-    sessionId <- liftIO createSessionId  --session ID
-    let outputPath = DT.unpack $ appTempDir $ appSettings app
-    let temporaryDirectoryPath = outputPath ++ sessionId ++ "/"
-    let geQueueName = DT.unpack $ appGeQueueName $ appSettings app
-    let tawsresultPath = temporaryDirectoryPath ++ "result.txt"
-    let dataPath = DT.unpack $ appDataDir $ appSettings app
-    let bigcachePath = dataPath ++ "U50_vs_SP.xml"
-    let programPath = DT.unpack $ appProgramDir $ appSettings app
-    liftIO (createDirectory temporaryDirectoryPath)
-    liftIO (writesubmissionData formResult temporaryDirectoryPath)
-    let inputPath = case sampleResult of FormSuccess sample -> (dataPath ++ (DT.unpack sample))
-                                         _ -> temporaryDirectoryPath ++ "input.fa"
-    uploadedFile <- liftIO (B.readFile inputPath)
-    let validatedInput = validateInput uploadedFile sampleResult
-    if (isRight validatedInput)
-        then do
-            --run blast to create xml
-            let blastpath = programPath ++ "blast/bin/"
-            let unirefpath = dataPath ++ "/uniref50.fasta"
-            let smallcachePath = temporaryDirectoryPath ++ "Inp_vs_U50.xml"
-            let taerrorPath = temporaryDirectoryPath ++ "errorMsg.txt"
-            let blastcommand = blastpath ++ "blastx -query "++ inputPath ++" -db " ++ unirefpath ++ " -evalue 1e-4 -num_threads 5 -outfmt 5 -out " ++ smallcachePath ++ "\n"
-            ----------------
-            --Submit RNAlien Job to SGE
-            --continue with samlesubmission xml file TODO change later!!!
-            let blastfilter = setBlastFilter formResult
-            let tacommand = programPath ++ "transalign +RTS -s -C -w -A100M -RTS " ++ blastfilter  ++ " " ++ smallcachePath ++ " " ++  bigcachePath  ++ " > " ++ tawsresultPath ++ " 2> " ++ taerrorPath ++ "\n"
-            let archivecommand = "zip -9 -r " ++  temporaryDirectoryPath ++ "result.zip " ++ temporaryDirectoryPath ++ "\n"
-            let blastdonecommand = "touch " ++ temporaryDirectoryPath ++ "/blastdone \n"
-            let blastbegincommand = "touch " ++ temporaryDirectoryPath ++ "/blastbegin \n"
-            let donecommand = "touch " ++ temporaryDirectoryPath ++ "/done \n"
-            let begincommand = "touch " ++ temporaryDirectoryPath ++ "/begin \n"
-            let delcommand = "rm -r " ++ smallcachePath ++ "\n"
-            let delcommanderr = "rm " ++ taerrorPath ++ "\n"
-            let blastdbpath = "export BLASTDB=" ++ dataPath ++ "uniref50.fasta \n"
-            --sun grid engine settings
-            let qsubLocation = "/usr/bin/qsub"
-            let geErrorDir = temporaryDirectoryPath ++ "gelog"
-            let geLogOutputDir = temporaryDirectoryPath ++ "gelog"
-            let bashscriptpath = temporaryDirectoryPath ++ "qsub.sh"
-            let bashheader = "#!/bin/bash\n"
-            let geJoinErrorsSwitch = "#$ -j y\n"
-            let geErrorPathSwitch = "#$ -e " ++ geErrorDir ++ "\n"
-            let geOutputPathSwitch = "#$ -o " ++ geLogOutputDir ++ "\n"            
-            let bashLDLibrary = "#$ -v LD_LIBRARY_PATH=" ++ dataPath ++ "\n"
-            let bashmemrequest = "#$ -l mem_free=25G\n"
-            --let bashhostrequest = "#$ -l hostname=\"picard\"\n" 
-            let parallelenv = "#$ -pe para 5\n"
-            let bashPath = "#$ -v PATH=" ++ programPath ++ ":/usr/bin/:/bin/:$PATH\n"
-            let bashcontent = bashheader ++ bashLDLibrary ++ geJoinErrorsSwitch ++ geErrorPathSwitch ++ geOutputPathSwitch ++ bashmemrequest ++ parallelenv ++ bashPath ++ blastdbpath ++ blastbegincommand ++ blastcommand ++ blastdonecommand ++ begincommand ++ tacommand ++ delcommand ++ delcommanderr ++ archivecommand ++ donecommand
-            let qsubcommand = qsubLocation ++ " -N " ++ sessionId ++ " -l h_vmem=25G " ++ " -q " ++ (geQueueName) ++ " " ++ bashscriptpath ++ " > " ++ temporaryDirectoryPath ++ "GEJobid"
-            liftIO (SI.writeFile geErrorDir "")
-            liftIO (SI.writeFile bashscriptpath bashcontent)
-            _ <- liftIO (runCommand (qsubcommand))
-            --Render page
-            defaultLayout $ do
-                aDomId <- newIdent
-                let approotjs = appRoot $ appSettings app
-                let sessionIdInsert =  DT.pack sessionId
-                let sessionIdjs = sessionId
-                $(widgetFile "calc")
-                setTitle "Welcome To TAWS!"
-        else do getHomeR
-                --hand over error message
+  ((formResult, _), _) <- runFormPost inputForm
+  ((sampleResult,_),_) <- runFormPost sampleForm
+  app <- getYesod -- contains all the generally set path variables
+  --Create tempdir and session-Id
+  sessionId <- liftIO createSessionId  --session ID
+  let outputPath = DT.unpack $ appTempDir $ appSettings app
+  let temporaryDirectoryPath = outputPath ++ sessionId ++ "/"
+  let geQueueName = DT.unpack $ appGeQueueName $ appSettings app
+  let tawsresultPath = temporaryDirectoryPath ++ "result.txt"
+  let dataPath = DT.unpack $ appDataDir $ appSettings app
+  let bigcachePath = dataPath ++ "U50_vs_SP.xml"
+  let programPath = DT.unpack $ appProgramDir $ appSettings app
+  liftIO (createDirectory temporaryDirectoryPath)
+  liftIO (writesubmissionData formResult temporaryDirectoryPath)
+  let inputPath = case sampleResult of FormSuccess sample -> (dataPath ++ (DT.unpack sample))
+                                       _ -> temporaryDirectoryPath ++ "input.fa"
+  uploadedFile <- liftIO (B.readFile inputPath)
+  let validatedInput = validateInput uploadedFile sampleResult
+  if (isRight validatedInput)
+    then do
+      --run blast to create xml
+      let blastpath = programPath ++ "blast/bin/"
+      let unirefpath = dataPath ++ "/uniref50.fasta"
+      let smallcachePath = temporaryDirectoryPath ++ "Inp_vs_U50.xml"
+      let taerrorPath = temporaryDirectoryPath ++ "errorMsg.txt"
+      let blastcommand = blastpath ++ "blastx -query "++ inputPath ++" -db " ++ unirefpath ++ " -evalue 1e-4 -num_threads 5 -outfmt 5 -out " ++ smallcachePath ++ "\n"
+      ----------------
+      --Submit RNAlien Job to SGE
+      --continue with samlesubmission xml file TODO change later!!!
+      let blastfilter = setBlastFilter formResult
+      let tacommand = programPath ++ "transalign +RTS -s -C -w -A100M -RTS " ++ blastfilter  ++ " " ++ smallcachePath ++ " " ++  bigcachePath  ++ " > " ++ tawsresultPath ++ " 2> " ++ taerrorPath ++ "\n"
+      let archivecommand = "zip -9 -r " ++  temporaryDirectoryPath ++ "result.zip " ++ temporaryDirectoryPath ++ "\n"
+      let blastdonecommand = "touch " ++ temporaryDirectoryPath ++ "/blastdone \n"
+      let blastbegincommand = "touch " ++ temporaryDirectoryPath ++ "/blastbegin \n"
+      let donecommand = "touch " ++ temporaryDirectoryPath ++ "/done \n"
+      let begincommand = "touch " ++ temporaryDirectoryPath ++ "/begin \n"
+      let delcommand = "rm -r " ++ smallcachePath ++ "\n"
+      let delcommanderr = "rm " ++ taerrorPath ++ "\n"
+      let blastdbpath = "export BLASTDB=" ++ dataPath ++ "uniref50.fasta \n"
+      --sun grid engine settings
+      let qsubLocation = "/usr/bin/qsub"
+      let geErrorDir = temporaryDirectoryPath ++ "gelog"
+      let geLogOutputDir = temporaryDirectoryPath ++ "gelog"
+      let bashscriptpath = temporaryDirectoryPath ++ "qsub.sh"
+      let bashheader = "#!/bin/bash\n"
+      let geJoinErrorsSwitch = "#$ -j y\n"
+      let geErrorPathSwitch = "#$ -e " ++ geErrorDir ++ "\n"
+      let geOutputPathSwitch = "#$ -o " ++ geLogOutputDir ++ "\n"            
+      let bashLDLibrary = "#$ -v LD_LIBRARY_PATH=" ++ dataPath ++ "\n"
+      let bashmemrequest = "#$ -l mem_free=25G\n"
+      --let bashhostrequest = "#$ -l hostname=\"picard\"\n" 
+      let parallelenv = "#$ -pe para 5\n"
+      let bashPath = "#$ -v PATH=" ++ programPath ++ ":/usr/bin/:/bin/:$PATH\n"
+      let bashcontent = bashheader ++ bashLDLibrary ++ geJoinErrorsSwitch ++ geErrorPathSwitch ++ geOutputPathSwitch ++ bashmemrequest ++ parallelenv ++ bashPath ++ blastdbpath ++ blastbegincommand ++ blastcommand ++ blastdonecommand ++ begincommand ++ tacommand ++ delcommand ++ delcommanderr ++ archivecommand ++ donecommand
+      let qsubcommand = qsubLocation ++ " -N " ++ sessionId ++ " -l h_vmem=25G " ++ " -q " ++ (geQueueName) ++ " " ++ bashscriptpath ++ " > " ++ temporaryDirectoryPath ++ "GEJobid"
+      liftIO (SI.writeFile geErrorDir "")
+      liftIO (SI.writeFile bashscriptpath bashcontent)
+      _ <- liftIO (runCommand (qsubcommand))
+      --Render page
+      defaultLayout $ do
+        aDomId <- newIdent
+        let approotjs = appRoot $ appSettings app
+        let sessionIdInsert =  DT.pack sessionId
+        let sessionIdjs = sessionId
+        $(widgetFile "calc")
+        setTitle "Welcome To TAWS!"
+    else do
+      getHomeR
+--      (formWidget, formEnctype) <- generateFormPost inputForm
+--      (sampleWidget, sampleEnctype) <- generateFormPost sampleForm
+--      defaultLayout $ do
+--        aDomId <- newIdent
+--        let errorMsg = DT.pack ((fromLeft validatedInput) ++ "<br>")
+--        setTitle "Welcome To TAWS!"
+--          $(widgetFile "homepage") 
 
 inputForm :: Form (Maybe FileInfo, Maybe Textarea, Maybe Text)
 inputForm = renderBootstrap3 BootstrapBasicForm $ (,,)
